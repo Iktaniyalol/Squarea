143b8f1edb56f49cd8eca3065a0d6841ee02b30c
Iktaniyalol <ilonellochka7@gmail.com>
2022-03-24 23:34:39 +1000

    Класс Server, его единичный экземпляр создается при запуске сервера. В будущем на нем будет ссылаться объект класса управления сетевой частью, чтобы он не очищался GC. Класс Игрока как заготовка.

---
+++ Server/Server.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using Server.Net;
+
+namespace Server
+{
+    public class Server
+    {
+        IPAddress IP = IPAddress.Parse("127.0.0.1");
+        int port = 27767; // порт для приема входящих запросов
+        ushort maxPlayers = 30; //максимальное количество игроков, может быть в будущем настроено через файл настройки сервера.
+        Player[] players;
+
+        public Server()
+        {
+            players = new Player[maxPlayers]; //Создаем массив игроков
+
+            //TODO различные загрузки
+            // Сеть
+            Console.WriteLine("Сервер запущен.");
+        }
+    }
+}
